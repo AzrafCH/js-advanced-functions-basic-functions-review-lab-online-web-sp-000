e958e06c0434a532497a4be41a4c9e51d415a226
Azraf Chowdhury <achowdhur@ltu.edu>
2020-06-25 10:11:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function saturdayFun(activity="roller-skate") {
 function mondayWork(activity="go to the office") {
   return `This Monday, I will ${activity}.`
 }
+
+function wrapAdjective(star='*') {
+  return function(adjective) {
+    return `${star}${adjective}${star}`
+  }
+}
