b4ebf92a723c78941eb0bbf9fddfb61aef361a2c
Azraf Chowdhury <achowdhur@ltu.edu>
2020-06-25 10:18:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ function wrapAdjective(flair='*') {
 }
 
 let Calculator = {
-  function add(num1, num2) {
-    return  num1 + num2
-  }
-
-  function subtract(num1, num2) {
+  add : function(num1, num2) {
+    return num1 +  num2
+  },
+  subtract : function(num1, num2) {
     return num1 - num2
-  }
-  function divide(num1, num2) {
+  },
+  multiply : function(num1, num2) {
+    return num1 * num2
+  },
+  divide : function(num1, num2) {
     return num1 / num2
   }
-  function multiply(num1, num2) {
-    return num1 ** num2
-  }
 }
+
